fd0078718f9e3767deab3da7251d3c880c7b16f2
Alejandro <alfaseart@gmail.com>
2022-08-01 14:25:18 +0200

    primer commit

---
+++ sockets/controller.js
@@ -0,0 +1,18 @@
+const socketController = (socket) => {
+
+    //console.log('Cliente conectado', socket.id);
+
+    socket.on('disconnect', () => {
+        //console.log('Cliente desconectado');
+    });
+
+    socket.on('mensaje', (payload, callback) => {
+        //console.log(payload)
+        socket.broadcast.emit('mensaje-recibido',payload)
+        const msg = 'Date por contestado';
+        callback(msg);
+    });
+
+}
+
+module.exports = { socketController } 
